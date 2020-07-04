914c6e5c85b62e7ca9f8d7c8ff0abb4f8fe38376
AntonioQM <antonioqm3@gmail.com>
2020-07-04 15:04:41 -0500

    desafio 02 finalizado com sucesso (penso eu)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import "./styles.css";
 
+import api from "services/api";
+
 function App() {
+  /**
+   * 'repositories' é uma nova constante para armazenar os dados
+   * 'setRepositories' é uma fução que é responsável por inserir os dados na variável 'repositories'
+   * 'useState' é uma função que serve para, via destruct, criar uma const que contém os dados e uma função que os seta, 
+   */
+
+  const [repositories, setRepositories] = useState([]); 
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      let { data } = response; 
+      setRepositories(data);
+    })
+  },[])
+  
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+        title: `Real DIgital 3 ${Date.now()}`,
+        url: "http://real.qm",
+        techs: [ "node.js", "js puro", "css", "vue js"]
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete('repositories/'+id,).then(response => {
+      console.log(response)
+      setRepositories(repositories.filter( repository => repository.id !== id));
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository =>  <li key={repository.id}>{repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
